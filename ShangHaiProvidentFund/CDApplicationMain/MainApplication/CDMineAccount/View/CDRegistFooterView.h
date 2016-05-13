180b5e29a9dab3a840810ad97b21b938bbaecb62
HelloWinter <cdd@cdddeiMac.local>
2016-05-13 19:18:26 +0800

    Removed file that shouldn't be tracked

---
+++ ShangHaiProvidentFund/CDApplicationMain/MainApplication/CDMineAccount/View/CDRegistFooterView.h
@@ -0,0 +1,13 @@
+//
+//  CDRegistFooterView.h
+//  ShangHaiProvidentFund
+//
+//  Created by cdd on 16/5/13.
+//  Copyright © 2016年 cheng dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDRegistFooterView : UIView
+
+@end
---
+++ ShangHaiProvidentFund/CDApplicationMain/MainApplication/CDMineAccount/View/CDRegistFooterView.m
@@ -0,0 +1,21 @@
+//
+//  CDRegistFooterView.m
+//  ShangHaiProvidentFund
+//
+//  Created by cdd on 16/5/13.
+//  Copyright © 2016年 cheng dong. All rights reserved.
+//
+
+#import "CDRegistFooterView.h"
+
+@implementation CDRegistFooterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
