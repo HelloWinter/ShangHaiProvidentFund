fccc62edcdeb790bc483a22e291e64d977678031
HelloWinter <cdd@cdddeiMac.local>
2016-07-20 17:54:35 +0800

    1.1.9

--- ShangHaiProvidentFund/CDGlobal/CDCustomModule/CDAutoHideMessageHUD/CDAutoHideMessageHUD.m
+++ ShangHaiProvidentFund/CDGlobal/CDCustomModule/CDAutoHideMessageHUD/CDAutoHideMessageHUD.m
@@ -14,8 +14,9 @@ static const CGFloat kVIEW_MAX_WIDTH = 260;
 static const CGFloat kVIEW_MAX_HEIGHT = 120;
 //static const CGFloat kVIEW_MIN_WIDTH = 60;
 static const CGFloat kVIEW_MIN_HEIGHT = 30;
+static const CGFloat klbTextFont = 14;
 
-static char CDAutoHideMessageHUDKey;
+static void * CDAutoHideMessageHUDKey = (void *)@"CDAutoHideMessageHUDKey";
 
 @implementation CDAutoHideMessageHUD
 
@@ -29,31 +30,30 @@ static char CDAutoHideMessageHUDKey;
 
 + (void)showMessage:(NSString *)msg inView:(UIView *)view duration:(NSTimeInterval)duration{
     if(view && msg && duration > 0){
-        CDNewLabel *label=(CDNewLabel *)objc_getAssociatedObject(view, &CDAutoHideMessageHUDKey);
+        CDNewLabel *label=(CDNewLabel *)objc_getAssociatedObject(view, CDAutoHideMessageHUDKey);
         if (label==nil) {
             label = [[CDNewLabel alloc]init];
             label.backgroundColor=[[UIColor blackColor]colorWithAlphaComponent:0.75f];
             label.textColor=[UIColor whiteColor];
             label.textAlignment = NSTextAlignmentCenter;
             label.layer.cornerRadius=5;
-            label.clipsToBounds=YES;
-            label.font=[UIFont systemFontOfSize:14];
+            label.layer.masksToBounds=YES;
+            label.font=[UIFont systemFontOfSize:klbTextFont];
             label.numberOfLines=0;
             label.lineBreakMode=NSLineBreakByWordWrapping;
             label.alpha=0;
             [view addSubview:label];
-            objc_setAssociatedObject(view, &CDAutoHideMessageHUDKey, label, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+            objc_setAssociatedObject(view, CDAutoHideMessageHUDKey, label, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
         }
         [view bringSubviewToFront:label];
         
-        CGSize lbSize=CGSizeZero;
-        CGRect rect = [msg boundingRectWithSize:CGSizeMake(kVIEW_MAX_WIDTH-20, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:[UIFont boldSystemFontOfSize:14]} context:nil];
-        lbSize=CGSizeMake(CGRectGetWidth(rect), CGRectGetHeight(rect));
-        lbSize.width+=20;
-//        lbSize.width = MIN(kVIEW_MAX_WIDTH, MAX(kVIEW_MIN_WIDTH, lbSize.width+20));
-        lbSize.height = MIN(kVIEW_MAX_HEIGHT, MAX(kVIEW_MIN_HEIGHT, lbSize.height+20));
-        label.bounds = CGRectMake(0, 0, ceilf(lbSize.width), ceilf(lbSize.height));
-        label.center = CGPointMake(view.bounds.size.width * 0.5f, view.bounds.size.height * 0.75f);
+        CGSize vwSize = view.bounds.size;
+        CGRect rect = [msg boundingRectWithSize:CGSizeMake(kVIEW_MAX_WIDTH-20, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:[UIFont boldSystemFontOfSize:klbTextFont]} context:nil];
+        //        CGFloat lbWidth = MIN(kVIEW_MAX_WIDTH, MAX(kVIEW_MIN_WIDTH, CGRectGetWidth(rect)+20));
+        CGFloat lbWidth=CGRectGetWidth(rect)+20;
+        CGFloat lbHeight=MIN(kVIEW_MAX_HEIGHT, MAX(kVIEW_MIN_HEIGHT, CGRectGetHeight(rect)+20));
+        label.bounds = CGRectMake(0, 0, ceilf(lbWidth), ceilf(lbHeight));
+        label.center = CGPointMake(vwSize.width * 0.5f, vwSize.height * 0.5f);
         label.text = msg;
         
         __weak __typeof(UILabel *) weakLabel=label;
@@ -65,13 +65,12 @@ static char CDAutoHideMessageHUDKey;
                     weakLabel.alpha=0;
                 } completion:^(BOOL finished) {
                     [weakLabel removeFromSuperview];
-                    objc_setAssociatedObject(view, &CDAutoHideMessageHUDKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+                    objc_setAssociatedObject(view, CDAutoHideMessageHUDKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
                 }];
             });
         }];
     }
 }
-
 
 @end
 
